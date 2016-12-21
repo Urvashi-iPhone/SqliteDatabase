ce32183e22b94dede41c1c8bdefc706f46e2cb35
Urvashi-iPhone <lathiyaurvashi36@gmail.com>
2016-12-21 11:59:10 +0530

    Initial Commit

---
+++ Sqlite/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sqlite
+//
+//  Created by Tecksky Techonologies on 12/21/16.
+//  Copyright © 2016 Tecksky Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
