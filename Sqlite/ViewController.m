6db31b1f42e7ec405e717b89a74607cc0824b009
Urvashi-iPhone <lathiyaurvashi36@gmail.com>
2016-12-21 13:12:37 +0530

    first

--- Sqlite/ViewController.m
+++ Sqlite/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
+#import "DBManager.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *nametxt;
+@property (weak, nonatomic) IBOutlet UIButton *submit;
+@property (weak, nonatomic) IBOutlet UITextField *searchTxt;
+@property (weak, nonatomic) IBOutlet UIButton *search;
+@property (weak, nonatomic) IBOutlet UILabel *serchname;
 
 @end
 
@@ -25,5 +31,22 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)savedata:(id)sender
+{
+    BOOL res = [[DBManager getSharedInstance] insertData:_nametxt.text];
+    if (res) {
+        NSLog(@"Success");
+    }
+    else
+    {
+        NSLog(@"Error");
+    }
+}
+- (IBAction)searchdata:(id)sender
+{
+    NSArray *records = [[DBManager getSharedInstance] searchStudent:[_searchTxt.text integerValue]];
+    NSLog(@"%@",records);
+    _serchname.text = [records objectAtIndex:0];
+}
 
 @end
